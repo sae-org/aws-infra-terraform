39cc37ec25c0b0cf085c0b5087a015f299e4f319
Saeeda <saeedabatool39@gmail.com>
2025-09-03 13:45:29 -0400

    added asg policy to scale based on cpu metrics

--- modules/ec2/asg.tf
+++ modules/ec2/asg.tf
@@ -33,3 +33,16 @@ resource "aws_autoscaling_group" "web_asg" {
     create_before_destroy = true
   }
 }
+
+resource "aws_autoscaling_policy" "cpu_target" {
+  name                   = "cpu-target-50"
+  autoscaling_group_name = aws_autoscaling_group.web_asg.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50 # scale to keep average CPU ~50%
+  }
+}
