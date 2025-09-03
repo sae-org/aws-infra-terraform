1018862f373e97645cfd8723f6a9c34ed1456e47
Saeeda <saeedabatool39@gmail.com>
2025-09-03 15:53:57 -0400

    added period attr to cw

--- modules/monitoring/cw.tf
+++ modules/monitoring/cw.tf
@@ -2,7 +2,8 @@ resource "aws_cloudwatch_metric_alarm" "asg_cpu_high" {
   alarm_name          = "${var.asg_name}-avg-cpu-high"
   alarm_description   = "ASG average CPU is above threshold"
   comparison_operator = "GreaterThanThreshold"
-  threshold           = 60               
+  threshold           = 65
+  period              = 60               
   evaluation_periods  = 3                 # 3 minutes window
   datapoints_to_alarm = 2                 # needs 2 of the 3 data points to breach
   treat_missing_data  = "notBreaching"
@@ -12,9 +13,16 @@ resource "aws_cloudwatch_metric_alarm" "asg_cpu_high" {
   # Metric math: average CPUUtilization across all EC2 instances in this ASG
   # Every 60s period, take the Average of all instance CPU metrics found by SEARCH
   metric_query {
-    id          = "e1"
-    label       = "ASG Avg CPU"
-    return_data = true
-    expression  = "AVG(SEARCH('{AWS/EC2,AutoScalingGroupName} MetricName=\"CPUUtilization\" AND AutoScalingGroupName=\"${var.asg_name}\"', 'Average', 60))"
-  }
+      id          = "m1"
+      return_data = true
+      metric {
+        namespace   = "AWS/EC2"
+        metric_name = "CPUUtilization"
+        stat        = "Average"
+        period      = 60
+        dimensions = {
+          AutoScalingGroupName = var.asg_name
+        }
+      }
+    }
 }
