226954a711d272a8e0476ecb21e550daaeccb9c4
Saeeda <saeedabatool39@gmail.com>
2025-09-16 14:43:00 -0400

    fixed user-data file path

--- modules/ec2/key_pair.tf
+++ modules/ec2/key_pair.tf
@@ -1,5 +1,5 @@
 # # created and added a new key pair which was then added to the instance ---------------------------------------------
-# resource "aws_key_pair" "newkey" {
-#   key_name   = "ansible-test"
-#   public_key = var.public_key
-# }
+resource "aws_key_pair" "newkey" {
+  key_name   = "ansible-test-git"
+  public_key = var.public_key
+}
