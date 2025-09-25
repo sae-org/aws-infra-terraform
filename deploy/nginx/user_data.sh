386827767abbc62ff2e9d83058c1f91c1ad8765a
Saeeda <saeedabatool39@gmail.com>
2025-09-25 14:09:59 -0400

    changed user data to run docker image if it exists

--- deploy/nginx/user_data.sh
+++ deploy/nginx/user_data.sh
@@ -15,3 +15,35 @@ if ! command -v aws >/dev/null 2>&1; then
   unzip -q /tmp/awscliv2.zip -d /tmp
   /tmp/aws/install --update
 fi
+
+# ----------------------------
+# App Docker Image Bootstrap
+# ----------------------------
+ACCOUNT_ID="886687538523"    
+REPO="my-dev-ecr-repo-1"          
+CONTAINER_NAME="myapp"
+HOST_PORT=80
+CONTAINER_PORT=80
+TAG="${IMAGE_TAG:-dev}"      
+
+# It calls the EC2 Metadata Service to get the instance’s Availability Zone (like us-east-1a) and saves it in AZ.
+AZ="$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone || true)"
+REGION="${AZ::-1}" # drop last letter from AZ 
+if [ -z "$REGION" ]; then # -z checks for empty strings 
+  REGION="us-east-1"
+fi
+REGISTRY="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
+
+# ECR login + pull
+aws ecr get-login-password --region "$REGION" \
+  | docker login --username AWS --password-stdin "$REGISTRY"
+
+docker pull "$REGISTRY/$REPO:$TAG"
+
+# Run/replace container 
+docker rm -f "$CONTAINER_NAME" >/dev/null 2>&1 || true
+docker run -d \
+  --name "$CONTAINER_NAME" \
+  --restart=always \
+  -p ${HOST_PORT}:${CONTAINER_PORT} \
+  "$REGISTRY/$REPO:$TAG"
